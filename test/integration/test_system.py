3683e1a67dbc6af18c7213d1a15f69c571fca3d5
rzuckerm <rzdulc@gmail.com>
2026-02-08 07:52:23 -0600

    Get information about each language

--- test/integration/test_system.py
+++ test/integration/test_system.py
@@ -2,12 +2,41 @@ from __future__ import annotations
 
 import os
 import subprocess
+from dataclasses import dataclass, field
 from pathlib import Path
 
-import yaml
 import pytest
+import yaml
 
 TEST_DATA_DIR = Path("test/integration/data/system-test").resolve()
+
+
+@dataclass
+class DockerInfo:
+    docker_image: str
+    language: str
+    files: list[str] = field(default_factory=list)
+
+
+def get_docker_info_list() -> list[DockerInfo]:
+    docker_info_list = []
+    for root, _, files in os.walk(TEST_DATA_DIR):
+        if "testinfo.yml" in files:
+            with Path(root, "testinfo.yml").open(encoding="utf-8") as f:
+                contents = yaml.safe_load(f)
+
+            extension = contents["folder_info"]["extension"]
+            container_info = contents["container_info"]
+            docker_info_list.append(
+                DockerInfo(
+                    docker_image=f"{container_info['image']}:{container_info['tag']}",
+                    language=Path(root).name,
+                    files=[str(Path(root, file_) for file_ in files if file_.endswith(extension))],
+                )
+            )
+
+    return docker_info_list
+
 
 @pytest.fixture(scope="module", autouse=True)
 def remove_docker_images():
@@ -17,20 +46,14 @@ def remove_docker_images():
     finally:
         _remove_docker_images()
 
-def get_docker_images() -> list[str]:
-    docker_images = []
-    for root, _, files in os.walk(TEST_DATA_DIR):
-        if "testinfo.yml" in files:
-            with Path(root, "testinfo.yml").open(encoding="utf-8") as f:
-                folder_info = yaml.safe_load(f)["folder"]
-
-            docker_images.append(f"{folder_info['image']}:{folder_info['tag']}")
-
-    return docker_images
 
 def _remove_docker_images() -> None:
-    docker_images = get_docker_images()
-    loaded_docker_images = subprocess.run(["docker", "images", "--format", "{{.Repository}}:{{.Tag}}"], capture_output=True, check=True).stdout.splitlines()
-    for docker_image in docker_images:
-        if docker_image in loaded_docker_images:
-            subprocess.run(["docker", "rmi", docker_image], check=True)
+    docker_info_list = get_docker_info_list()
+    loaded_docker_images = subprocess.run(
+        ["docker", "images", "--format", "{{.Repository}}:{{.Tag}}"],
+        capture_output=True,
+        check=True,
+    ).stdout.splitlines()
+    for docker_info in docker_info_list:
+        if docker_info.docker_image in loaded_docker_images:
+            subprocess.run(["docker", "rmi", docker_info.docker_image], check=True)
