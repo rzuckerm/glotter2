3e29a68c330c4e45fb0ea7e818f98c4bff5f1d5c
rzuckerm <rzuckerm@users.noreply.github.com>
2026-02-08 12:48:21 -0600

    Add system tests (#34)
    Also update dependencies

---
+++ test/integration/data/system-test/archive/c/c-plus-plus/hello-world.cpp
@@ -0,0 +1,10 @@
+// Source:
+// https://github.com/TheRenegadeCoder/sample-programs/blob/main/archive/c/c-plus-plus/hello-world.cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!";
+    return 0;
+}
---
+++ test/integration/data/system-test/archive/p/python/hello_world.py
@@ -0,0 +1,3 @@
+# Source:
+# https://github.com/TheRenegadeCoder/sample-programs/blob/main/archive/p/python/hello_world.py
+print('Hello, World!')
